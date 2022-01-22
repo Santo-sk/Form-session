9b0ff20212e9ea76ab5c796b19ee3fb49c5be6cc
santo-lap <santhoshkanna98@gmail.com>
2022-01-22 14:40:16 +0530

    changes in home page with bg

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -8,37 +9,50 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class SignUpComponent implements OnInit {
 
-  firstName:String;
-  lastName:String;
-  mailId:String;
-  phoneNo:number;
-  dob:String;
-  gender:String;
+  // firstName:String;
+  // lastName:String;
+  // mailId:String;
+  // phoneNo:number;
+  // dob:String;
+  // gender:String;
 
+  userForm: NgForm | undefined;
 
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.gender='M';
+    // this.gender='M';
   }
 
-  saveDetails(){
+//   saveDetails(){
    
-let userdetails = {
-fName:this.firstName,
-lName:this.lastName,
-email:this.mailId,
-phone:this.phoneNo,
-dob:this.dob,
-gender:this.gender
-}
-sessionStorage.setItem('user-detail',JSON.stringify(userdetails));
+// let userdetails = {
+// fName:this.firstName,
+// lName:this.lastName,
+// email:this.mailId,
+// phone:this.phoneNo,
+// dob:this.dob,
+// gender:this.gender
+// }
+// sessionStorage.setItem('user-detail',JSON.stringify(userdetails));
+
+// this.snackBar.open('Saved Successfully','',
+//     { 
+//       duration: 1000,
+//       panelClass: ['snackbar-success']
+//   });
+
+//   }
+
+  onSubmit(userForm: NgForm){
+    sessionStorage.setItem('user-detail-template',JSON.stringify(userForm.value));
 
 this.snackBar.open('Saved Successfully','',
     { 
-      duration: 1000,
-      panelClass: ['snackbar-success']
+      duration: 2000,
+      panelClass: ['snackbar-success'],
+      verticalPosition: 'top'
   });
-
+  
   }
 }
